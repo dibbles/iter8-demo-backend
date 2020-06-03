f321c06959a078bac56b2a2c46a04b3a4666dee4
dibbles <dibbles@users.noreply.github.com>
2020-06-03 10:12:15 +0100

    v101 - 300ms

--- user-app/app.js
+++ user-app/app.js
@@ -4,8 +4,8 @@ const sleep = require('sleep')
 const app = express()
 
 app.get('/', (req, res) => {
-  sleep.msleep(100)
-  res.send('v1.0.0 thinks for 100 miliseconds!!');
+  sleep.msleep(300)
+  res.send('v1.0.1 thinks for 300 miliseconds!!');
 });
 
 app.listen(3000, '0.0.0.0')
